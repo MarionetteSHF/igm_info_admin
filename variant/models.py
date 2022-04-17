ac2f944567fdfac3a8649ecd120a9e0a814191ed
zheyanliu99 <zl3119@columbia.edu>
2022-04-17 16:27:49 -0400

    add variant models, now it can show in admin page

--- variant/models.py
+++ variant/models.py
@@ -1,3 +1,19 @@
-from django.db import models
-
 # Create your models here.
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class variant(models.Model):
+    local_id = models.CharField(max_length=200, null=True)
+    linking_id = models.CharField(max_length=200, primary_key=True)
+
+    # gene
+    reference_sequence = models.CharField(max_length=200)
+
+    # variant definition
+    hgvs = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.linking_id
