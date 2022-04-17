2f2cb919f96252de03df1e246c590588b0654be6
MarionetteSHF <hanfu.shi2019@gmail.com>
2022-04-17 15:38:56 -0400

    1112

--- DSI/settings.py
+++ DSI/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'DSI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': []
+# os.path.join(BASE_DIR, 'templates')
         ,
         'APP_DIRS': True,
         'OPTIONS': {
